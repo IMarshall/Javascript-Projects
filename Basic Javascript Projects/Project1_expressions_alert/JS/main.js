aa9377279beec9cf9af36a4a9de84db47f5d39cd
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-01-13 18:59:46 -0700

    Started the Javascript Course

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+var John = "Stockton", Karl = "Malone", Brian = "Russell", Greg = "Ostertag", Jeff = "Hornacek"; //defining multiple variables at once
+
+window.alert("Howdy ho, neighborino!")
+
+document.write(John.fontcolor("blue") + " 2 " + Karl.fontcolor("purple")); //a statement containing an expression that concetenates 2 variables and a string. The variables use the font color method to change the color of text in their strings.
