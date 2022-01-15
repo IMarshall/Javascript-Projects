e7a8407dedf4352b5c4ddaa6d34fed4601ec3da3
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-01-14 17:47:49 -0700

    Added Project2_functions folder

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -3,3 +3,7 @@ var John = "Stockton", Karl = "Malone", Brian = "Russell", Greg = "Ostertag", Je
 window.alert("Howdy ho, neighborino!")
 
 document.write(John.fontcolor("blue") + " 2 " + Karl.fontcolor("purple")); //a statement containing an expression that concetenates 2 variables and a string. The variables use the font color method to change the color of text in their strings.
+
+function popup() {
+    window.alert("Hi there.");
+}
---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function add(num1, num2) {
+    var sum = (num1 + num2);
+    sum += " is the answer!"
+    document.getElementById("answer").innerHTML = sum;
+}
