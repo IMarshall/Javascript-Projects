978793a07e85e7a446c83891e19bac333002568a
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-01-14 19:31:43 -0700

    Update main.js

--- Basic Javascript Projects/Project4_dictionaries/JS/main.js
+++ Basic Javascript Projects/Project4_dictionaries/JS/main.js
@@ -1,3 +1,4 @@
+//define Instrument dictionary
 var Instrument = {
     Name: "Trumpet",
     Family: "Brass",
@@ -5,14 +6,17 @@ var Instrument = {
     Mouthpiece: "2C"
 };
 
+//define function to get the name of the instrument
 function trumpet() {
     document.getElementById("instrumentName").innerHTML = Instrument.Name;
 }
 
+//define function to delete the name of the instrument
 function deleteName() {
     delete Instrument.Name;
 }
 
+//define function to try to get the name of the instrument again
 function trumpet2() {
     document.getElementById("instrumentName2").innerHTML = Instrument.Name;
 }
