33152cf73ce1cea509fed9083cb8e711452c4184
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-01-17 16:34:40 -0700

    Finished Project6 Folder

--- Basic Javascript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic Javascript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,29 +1,35 @@
-function Vehicle(Make, Model, Year, Color) {
-    this.Vehicle_Make = Make;
-    this.Vehicle_Model = Model;
-    this.Vehicle_Year = Year;
-    this.Vehicle_Color = Color;
-}
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
-var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+//Define variables with random numbers to be used with ternary operators
+var x = Math.round(Math.random() * 100);
+var y = Math.round(Math.random() * 100);
 
-function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML =
-        "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
-        " manufactured in " + Erik.Vehicle_Year;
-}
+//Use of ternary operators to display correct answer
+window.alert(z = (x > y) ? x + " is more than " + y : x + " is less than " + y);
 
-function createVehicle() {
-    var Ian = new Vehicle("Porsche", "911", 2000, "Orange");
-    document.getElementById("New_and_This").innerHTML =
-        "New " + Ian.Vehicle_Color + " " + Ian.Vehicle_Make + " " + Ian.Vehicle_Model +
-        " created for Ian.";
-}
-
-function Instument(Name, Family, Valves, Reed_Size) {
+//Constructor function using "this" keyword
+function Instrument(Name, Family, Valves, Reed_Size) {
     this.Instument_Name = Name;
     this.Instrument_Family = Family;
     this.Valve_Num = Valves;
     this.Reed_Size = Reed_Size;
+}
+
+//Defining new instruments using the constructor function and "new" keyword
+var Trumpet = new Instrument("Trumpet", "Brass", 3, "NA");
+var Saxophone = new Instrument("Saxophone", "Woodwind", "NA", 2.5);
+
+//Function to display the results of the constructor function
+function createInstrument() {
+    var Tuba = new Instrument("Tuba", "brass", 4, "NA");
+    document.getElementById("New_and_This").innerHTML =
+        "Created new " + Tuba.Instrument_Family + " instrument called a " +
+        Tuba.Instument_Name + ", which has " + Tuba.Valve_Num + " valves.";
+}
+
+//Nested Function
+function roll() {
+    document.getElementById("Nested_Function").innerHTML = nested();
+    function nested() {
+        var num1 = Math.round(Math.random() * 6);
+        return num1;
+    }
 }
