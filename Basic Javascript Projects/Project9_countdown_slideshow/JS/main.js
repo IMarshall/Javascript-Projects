3cab36efde6571bd6b294ae61a4a6c2fddbe095c
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-01-19 17:00:02 -0700

    Finished Project9 Folder

--- Basic Javascript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic Javascript Projects/Project9_countdown_slideshow/JS/main.js
@@ -11,16 +11,13 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+//Function to display the correct slide and the correct active dot
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    console.log(slides);
-    console.log(slides.length);
-    console.log(dots);
-    console.log(dots[0]);
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { slideIndex = 1 } //wrap back to first image after third
+    if (n < 1) { slideIndex = slides.length } //wrap to last image after first
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -29,4 +26,23 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+}
+
+
+//Countdown function
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+    var timer = document.getElementById("timer");
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
 }
---
+++ Basic Javascript Projects/The_Countdown_Challenge/JS/main.js
@@ -0,0 +1,16 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+    var timer = document.getElementById("timer");
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
